4ca672712b0f5acd51e1fe2ff963d4704a04941d
tay-rel <sirenita0197@gmail.com>
2022-10-19 20:18:37 +0200

    actualizado shop

--- proyecto12/app/controllers/ShopController.php
+++ proyecto12/app/controllers/ShopController.php
@@ -8,18 +8,27 @@ class ShopController extends Controller
     {
         $this->model = $this->model('Shop');
     }
+
     public function index()
     {
         $session=new Session();
         if ($session->getLogin()){
             $data=[
                 'titulo'=>'Bienvenido a la tienda',
-                'menu'=>false,
+                'menu'=>true,
+                'subtitle'=>'Bienvenido a la tienda',
             ];
-            $this->view('shop/index');
+            $this->view('shop/index', $data);
         }else{
             header('LOCATION' . ROOT);
         }
 
+    }
+
+    public function logout()
+    {
+        $session = new Session();
+        $session->logout();
+        header('location:' . ROOT);
     }
 }
