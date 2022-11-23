4ca382ded2a9b6b034c26cfc2d001077839cac60
tay-rel <sirenita0197@gmail.com>
2022-11-23 20:16:37 +0100

    paso 38 modulo del usuario

--- proyecto13/resources/views/users/index.blade.php
+++ proyecto13/resources/views/users/index.blade.php
@@ -10,6 +10,12 @@
             <li>
                 {{ $user->name }} ({{ $user->email }})
                 <a href="{{ route('user.show', $user->id) }}">Ver detalles</a>
+                <a href="{{ route('users.edit', $user) }}">Editar</a>
+                <form action="{{ route('user.destroy', $user) }}" method="post">
+                    {{ csrf_field() }}
+                    {{ method_field('DELETE') }}
+                    <button type="submit">Eliminar</button>
+                </form>
             </li>
         @empty
             <p>No hay usuarios registrados</p>
