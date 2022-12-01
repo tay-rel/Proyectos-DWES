47633580184031ced261dea0f2244c78c6d068fe
tay-rel <sirenita0197@gmail.com>
2022-12-01 16:46:32 +0100

    paso 8

--- proyecto13/tests/TestCase.php
+++ proyecto13/tests/TestCase.php
@@ -6,14 +6,12 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
-    protected function assertDatabaseEmpty($table, $connection = null)
-    {
-        $total = $this->getConnection($connection)->table($table)->count();
+    use CreatesApplication, TestHelpers;
 
-        $this->assertSame(0, $total, sprintf(
-            "Failed asserting the table [%s] is empty. %s %s found.",
-            $table, $total, str_plural('row', $total)
-        ));
+    protected $defaultData;
+    public function setUp()
+    {
+        parent::setUp();
+        $this->withoutExceptionHandling();
     }
 }
