b1dc40c62275c3a740216819fae24827b20ef1d5
tay-rel <sirenita0197@gmail.com>
2022-12-01 17:16:40 +0100

    paso 12

--- proyecto13/resources/views/users/edit.blade.php
+++ proyecto13/resources/views/users/edit.blade.php
@@ -4,21 +4,23 @@
 
 @section('content')
 
-<h1>Editar usuario</h1>
+    @card
+        @slot('header', 'Editar usuario')
+            @include('shared._errors')
 
-@include('shared._errors')
+            <form action="{{ route('user.update', $user) }}" method="post">
+                {{method_field('PUT')}}
 
-<form action="{{ route('user.update', $user) }}" method="post">
-    {{method_field('PUT')}}
-    @include('users._fields')
+                    @include('users._fields')
 
-    <div class="form-group mt-4">
-        <button type="submit" class="btn btn-primary">Actualizar usuario</button>
-        <a href="{{ route('users') }}" class="btn btn-link">Regresar al listado de usuarios</a>
-    </div>
-</form>
+                <div class="form-group mt-4">
+                    <button type="submit" class="btn btn-primary">Actualizar usuario</button>
+                    <a href="{{ route('users') }}" class="btn btn-link">Regresar al listado de usuarios</a>
+                </div>
+            </form>
+    @endcard
 
-<p>
-    <a href="{{ route('users') }}">Regresar al listado de usuarios</a>
-</p>
+            <p>
+                <a href="{{ route('users') }}">Regresar al listado de usuarios</a>
+            </p>
 @endsection;
