1a39c1eb3778c4b61b3ee1e05f4e8fe3e1d6b4c4
tay-rel <sirenita0197@gmail.com>
2022-12-04 21:54:08 +0100

    paso 8

---
+++ proyecto13/app/Role.php
@@ -0,0 +1,10 @@
+<?php
+namespace App;
+
+class Role
+{
+    public static function getList()
+    {
+        return ['admin', 'user'];
+    }
+}
---
+++ proyecto13/database/factories/UserProfileFactory.php
@@ -0,0 +1,10 @@
+<?php
+
+use Faker\Generator as Faker;
+
+
+$factory->define(App\UserProfile::class, function (Faker $faker) {
+    return [
+        'bio' => $faker->paragraph,
+    ];
+});
