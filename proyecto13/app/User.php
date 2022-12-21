acffeb0d944ff922d8713ace045ddcaa954f8ee7
tay-rel <sirenita0197@gmail.com>
2022-12-21 19:13:43 +0100

    Paso 36 completo

--- proyecto13/app/User.php
+++ proyecto13/app/User.php
@@ -1,98 +1,77 @@
 <?php
 
-	namespace App;
+namespace App;
 
-	use Illuminate\Database\Eloquent\SoftDeletes;
-	use Illuminate\Notifications\Notifiable;
-	use Illuminate\Foundation\Auth\User as Authenticatable;
-	use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-	class User extends Authenticatable
-	{
-		use Notifiable, SoftDeletes;
+class User extends Authenticatable
+{
+    use Notifiable, SoftDeletes;
 
-		/**
-		 * The attributes that are mass assignable.
-		 *
-		 * @var array
-		 */
-		protected $guarded = [];
-        protected $casts = [
-            'active' => 'bool'
-        ];
-		/**
-		 * The attributes that should be hidden for arrays.
-		 *
-		 * @var array
-		 */
-		protected $hidden = ['password', 'remember_token'];
+    protected $guarded = [];
 
-		public static function findByEmail($email)
-		{
-			return static::whereEmail($email)->first();
-		}
-        public function getNameAttribute()
-        {
-            return $this->first_name . ' ' . $this->last_name;
+    protected $casts = [
+        'active' => 'bool'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token'
+    ];
+
+    /**
+     * Create a new Eloquent query builder for the model.
+     *
+     * @param  \Illuminate\Database\Query\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder|static
+     */
+    public function newEloquentBuilder($query)
+    {
+        return new UserQuery($query);
+    }
+
+
+    public function profile()
+    {
+        return $this->hasOne(UserProfile::class)->withDefault();
+    }
+
+    public function skills()
+    {
+        return $this->belongsToMany(Skill::class);
+    }
+
+    public function team()
+    {
+        return $this->belongsTo(Team::class)->withDefault();
+    }
+
+    public function isAdmin()
+    {
+        return $this->role === 'admin';
+    }
+
+    public function getNameAttribute()
+    {
+        return $this->first_name . ' ' . $this->last_name;
+    }
+
+    public function getStateAttribute()
+    {
+        if ($this->active != null) {
+            return $this->active ? 'active' : 'inactive';
         }
-        public function getStateAttribute()
-        {
-            if ($this->active != null) {
-                return $this->active ? 'active' : 'inactive';
-            }
-        }
+    }
 
-        public function setStateAttribute($value)
-        {
-            $this->attributes['active'] = $value == 'active';
-        }
-
-        public function scopeSearch($query, $search)
-        {
-            if (empty($search)) {
-                return;
-            }
-            $query->whereRaw('CONCAT(first_name, " ", last_name) like ?', "%{$search}%")
-                ->orWhere('email', 'like', "%{$search}%")
-                ->orWhereHas('team', function ($query) use ($search) {
-                    $query->where('name', 'like', "%{$search}%");
-                });
-        }
-
-        public function scopeByState($query, $state)
-        {
-            if ($state == 'active') {
-                return $query->where('active', true);
-            }
-
-            if ($state == 'inactive') {
-                return $query->where('active', false);
-            }
-        }
-
-		public function profile()
-		{
-			return $this->hasOne(UserProfile::class)->withDefault();
-		}
-
-		public function skills()
-		{
-			return $this->belongsToMany(Skill::class);
-		}
-
-		public function team()
-		{
-			return $this->belongsTo(Team::class)->withDefault();
-		}
-
-		public function isAdmin()
-		{
-			return $this->role === 'admin';
-		}
-        public function scopeByRole($query, $role)
-        {
-            if (in_array($role, ['admin', 'user'])) {
-                $query->where('role', $role);
-            }
-        }
-	}
+    public function setStateAttribute($value)
+    {
+        $this->attributes['active'] = $value == 'active';
+    }
+}
